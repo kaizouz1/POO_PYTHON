1e45287fcdfc211fda2c97814aaea82b5428ba7c
Kaizouz1 <joaoleiteferraco@gmail.com>
2026-02-04 17:23:57 -0300

    Add files via upload

---
+++ espaço.py
@@ -0,0 +1,21 @@
+import os
+def limpar():
+    os.system("cls")
+limpar()
+
+
+def epar(a):
+    par = (a % 2 == 0)
+    return par
+print(epar(2))
+
+print("===" * 20)
+
+# Para retornar par ou impar:
+def par_ou_impar(a):
+    if epar(a):
+        return "par"
+    else: 
+        return "impar"
+print(par_ou_impar(4))
+# arroz com feijao
